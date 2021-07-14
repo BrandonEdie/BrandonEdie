b4bda95ce4602696854baae21d1b34f40977f69f
BrandonEdie <82047545+BrandonEdie@users.noreply.github.com>
2021-07-14 15:19:54 -0400

    Update ruby.rb

--- ruby.rb
+++ ruby.rb
@@ -6,7 +6,7 @@ uri = URI.parse(url)
 
 http = Net::HTTP.new(uri.host, uri.port)
 request = Net::HTTP::Post.new(uri.request_uri, {'Content-Type': 'text/json'})
-request.body = ({"data": {"City":"","Password":"","Last Name":"","First Name":""}}).to_json
+request.body = ({"data": {"Email":"","Password":"","Last Name":"","First Name":""}}).to_json
 
 response = http.request(request)
 if response.code == '201'
