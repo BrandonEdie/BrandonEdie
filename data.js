66dd6e9e7092ae4d50503ff8b708ce8f3d9c4c5c
BrandonEdie <82047545+BrandonEdie@users.noreply.github.com>
2021-07-14 15:20:04 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 fetch("https://api.apispreadsheets.com/data/15497/", {
 	method: "POST",
-	body: JSON.stringify({"data": {"First Name":"","Last Name":"","City":"","Password":""}}),
+	body: JSON.stringify({"data": {"First Name":"","Last Name":"","Email":"","Password":""}}),
 }).then(res =>{
 	if (res.status === 201){
 		// SUCCESS
