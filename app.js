79ffd3a1fe5a5cd2640bec5f762c966a8d45ba7f
BrandonEdie <82047545+BrandonEdie@users.noreply.github.com>
2021-08-19 18:14:52 -0400

    Update app.js
    function sendEmail() {
      var element = {
    
        name: document.getElementById("name").value,
        email: document.getElementById("email").value,
        message: document.getElementById("message").value,
    };
      Email.send({
        Host: "smtp.gmail.com",
        Username: "brandonedie0000@gmail.com",
        Password: "eancwrxosftfhtyr",
        From: "brandonedie0000@gmail.com",
        To: element.email,
        Subject:"New Message From Brandon's Website",
        Body: "<strong> Thank You For Contacting Me The Website Is Sill Currently Under Contruction But You Can Revisit To View For Further Updates </strong>"
      })
    
      Email.send({
        Host: "smtp.gmail.com",
        Username: "brandonedie0000@gmail.com",
        Password: "eancwrxosftfhtyr",
        From: "brandonedie0000@gmail.com",
        To: "brandonedie0000@gmail.com",
        Subject:"New Message From Brandon's Website",
        Body: "<h1>"+element.name+"</h1>"+"<h2>"+element.email+"</h2>"+"<h3>"+element.message+"</h3>"
      })
    
      .then(function (message) {
        alert("mail sent successfully")
      });
    }

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
-function sendMail(params) {
-    var tempParams = {
 
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value,
-    };
-
-    emailjs.send('gmail', 'template_jw4whhg', tempParams)
-        .then(function(res){
-            console.log('success', res.status);
-            alert('Hey Jesus Christ Loves You');
-        })
-    }
